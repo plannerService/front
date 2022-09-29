5bdecc7bd81496e5eede40b359adc7bd223018d2
Song <misosjm@naver.com>
2022-09-30 01:52:21 +0900

    fdsafdsa

--- scripts/afterInstall.sh
+++ scripts/afterInstall.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-REPOSITORY = /home/ubuntu/root
+REPOSITORY="/home/ubuntu/root"
 
 echo "> docker-compose 파일이 있는 디렉토리로 이동"
 cd $REPOSITORY
@@ -9,4 +9,4 @@ echo "> docker-compose build 실행"
 sudo docker-compose up --build -d
 
 echo "> <none> image 삭제"
-sudo docker image prune
+sudo docker image prune -a
--- scripts/applicationStop.sh
+++ scripts/applicationStop.sh
@@ -1,4 +1,9 @@
 #! /bin/bash
 
 echo "> docker container 종료 및 삭제"
-sudo docker rm -f $(sudo docker ps -qa)
+
+back_container_name="root-back-1"
+nginx_container_name="root-nginx-1"
+
+sudo docker rm -f ${back_container_name}
+sudo docker rm -f ${nginx_container_name}
